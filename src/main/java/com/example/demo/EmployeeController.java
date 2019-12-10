cb7339163c0ab74c5101e1745b58a3c066e80e8b
Ronan Heffernan <rheffernan.ctr@kr.af.mil>
2019-12-10 16:43:58 -0500

    initial commit

---
+++ src/main/java/com/example/demo/EmployeeController.java
@@ -0,0 +1,93 @@
+package com.example.demo;
+
+import java.util.Comparator;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.stream.Collectors;
+import java.util.Comparator;
+
+//import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
+
+import org.springframework.hateoas.server.mvc.ControllerLinkRelationProvider;
+//note: this version of the hateos library does not have the deprecated classes from the tutorial
+
+
+@RestController
+class EmployeeController {
+
+    private final EmployeeRepository repository;
+
+    EmployeeController(EmployeeRepository repository) {
+        this.repository = repository;
+    }
+
+    // Aggregate root
+
+    @GetMapping("/employees")
+    List<Employee> all() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/estream")
+    List<Employee> efilter() {
+        //return repository.findAll().stream().filter(e -> e.getId() == 2).collect(Collectors.toList());
+        return repository.findAll().stream().filter(e -> e.getId() > 0).collect(Collectors.toList());
+    }
+
+    @GetMapping("/max")
+    Employee maxId() {
+        //return repository.findAll().stream().filter(e -> e.getId() == 2).collect(Collectors.toList());
+        return repository.findAll().stream().max(Comparator.comparingLong(Employee::getId)).get();
+    }
+
+    @PostMapping("/employees")
+    Employee newEmployee(@RequestBody Employee newEmployee) {
+        return repository.save(newEmployee);
+    }
+
+    // Single item (HATEOAS version)
+    /*@GetMapping("/employees/{id}")
+    Resource<Employee> one(@PathVariable Long id) {
+
+        Employee employee = repository.findById(id)
+                .orElseThrow(() -> new EmployeeNotFoundException(id));
+
+        return new Resource<>(employee,
+                linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
+                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+    }*/
+
+    @GetMapping("/employees/{id}")
+    Employee one(@PathVariable Long id) {
+
+        return repository.findById(id)
+                .orElseThrow(() -> new EmployeeNotFoundException(id));
+    }
+
+    @PutMapping("/employees/{id}")
+    Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
+
+        return repository.findById(id)
+                .map(employee -> {
+                    employee.setName(newEmployee.getName());
+                    employee.setRole(newEmployee.getRole());
+                    return repository.save(employee);
+                })
+                .orElseGet(() -> {
+                    newEmployee.setId(id);
+                    return repository.save(newEmployee);
+                });
+    }
+
+    @DeleteMapping("/employees/{id}")
+    void deleteEmployee(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+}
---
+++ src/main/java/com/example/demo/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+
+}
