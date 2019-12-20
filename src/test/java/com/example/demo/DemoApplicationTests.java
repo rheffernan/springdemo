a3bab0556f5dd5d42e7d1f629c0c29fbeef1651e
Ronan Heffernan <rheffernan.ctr@kr.af.mil>
2019-12-20 08:07:55 -0500

    trying to add integration tests

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,31 @@
 package com.example.demo;
 
+
+import io.restassured.RestAssured.*;
+import io.restassured.matcher.RestAssuredMatchers.*;
+import org.hamcrest.Matchers.*;
+
 import org.junit.jupiter.api.Test;
+import io.restassured.itest.java.support.WithJetty;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
-class DemoApplicationTests {
+@RunWith(SpringJUnit4ClassRunner.class)
+class DemoApplicationTests extends WithJetty {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void BodyCheck() {
+		get("/employees").then().body("class", equalTo("thief"));
+	}
+
+	@Test public void
+	BodyTest2() {
+		given().body("hullo").expect().statusCode(200).when().get("/getWithContent");
 	}
 
 }
