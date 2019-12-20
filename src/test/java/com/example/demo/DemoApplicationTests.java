d4e0ba4f7414729879b701dbc71dd06c14f72d8b
Ronan Heffernan <rheffernan.ctr@kr.af.mil>
2019-12-20 16:38:55 -0500

    fixed integration tests to work (and actually in a running context, not as a unit test)

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,18 +1,32 @@
 package com.example.demo;
 
 
-import io.restassured.RestAssured.*;
-import io.restassured.matcher.RestAssuredMatchers.*;
-import org.hamcrest.Matchers.*;
+//import io.restassured.RestAssured.*;
+//import io.restassured.matcher.RestAssuredMatchers.*;
+//import org.hamcrest.Matchers.*;
 
 import org.junit.jupiter.api.Test;
-import io.restassured.itest.java.support.WithJetty;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.stream.Collectors;
+
+//import io.restassured.itest.java.support.WithJetty;
 
 
-@SpringBootTest
-@RunWith(SpringJUnit4ClassRunner.class)
-class DemoApplicationTests extends WithJetty {
+//@SpringBootTest
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringApplicationConfiguration
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@SpringBootTest
+class DemoApplicationTests {
+
+	@Autowired
+	private EmployeeController employeeController;
 
 	@Test
 	void contextLoads() {
@@ -20,12 +34,12 @@ class DemoApplicationTests extends WithJetty {
 
 	@Test
 	void BodyCheck() {
-		get("/employees").then().body("class", equalTo("thief"));
+		assert(employeeController.all().stream().filter(e -> e.getName().equals("Bilbo Baggins")).collect(Collectors.toList()).size() >0);
 	}
 
 	@Test public void
 	BodyTest2() {
-		given().body("hullo").expect().statusCode(200).when().get("/getWithContent");
+		assert(employeeController.maxId().getName().equals("Frodo Baggins"));
 	}
 
 }
