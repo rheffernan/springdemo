5d8487f7c7ee2288ce4cc0d0551cdf8f51578405
Ronan Heffernan <rheffernan.ctr@kr.af.mil>
2019-12-23 17:37:25 -0500

    updating to be able to host the react files, and add more integration tests

--- src/main/java/com/example/demo/EmployeeController.java
+++ src/main/java/com/example/demo/EmployeeController.java
@@ -1,22 +1,13 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.*;
+
 import java.util.Comparator;
 import java.util.List;
-
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 import java.util.stream.Collectors;
-import java.util.Comparator;
 
 //deprecated:
 //import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
-
-import org.springframework.hateoas.server.mvc.ControllerLinkRelationProvider;
 //note: this version of the hateos library does not have the deprecated classes from the tutorial
 
 
@@ -31,7 +22,7 @@ class EmployeeController {
 
     // Aggregate root
 
-    @GetMapping("/employees")
+    @GetMapping("employees")
     List<Employee> all() {
         return repository.findAll();
     }
